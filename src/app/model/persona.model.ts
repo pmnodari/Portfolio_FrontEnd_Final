0d1e65fb47c607c468aaded7643944299e71e210
pmnodari <pmnodari@gmail.com>
2022-12-16 08:08:44 -0300

    Se termina de configurar la sección Acerca de mí.

--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -3,12 +3,14 @@ export class Persona {
     nombre:String;
     apellido: String;
     profecion: String;
+    descripcion: String
     img: String;
 
-    constructor(nombre: String, apellido: String, profecion: String, img: String){
+    constructor(nombre: String, apellido: String, profecion: String, descripcion: String, img: String){
         this.nombre=nombre;
         this.apellido=apellido;
         this.profecion=profecion;
+        this.descripcion=descripcion;
         this.img=img;
     }
 }
