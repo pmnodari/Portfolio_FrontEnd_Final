4eb4c7f22308a162cd6fc06be6bbb9dc1a3074f7
pmnodari <pmnodari@gmail.com>
2023-01-05 14:07:36 -0300

    Corrigiendo, y mejorando aspectos de diseño

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'PFinal_Pablo_Nodari';
+  title = 'Portfolio_Pablo_Nodari';
 }
